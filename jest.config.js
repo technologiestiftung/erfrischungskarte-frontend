9dd0bd344372bc145e8e274527db6a0a64973d1b
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-06-12 12:04:18 +0200

    test: setup jest-dom

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,5 @@ module.exports = {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
 }
