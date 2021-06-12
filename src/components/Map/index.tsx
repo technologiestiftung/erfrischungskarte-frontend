1b6dd1290af8d88ced9403b6b78b25c27758e3fa
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-06-12 14:28:08 +0200

    chore(Map): make map children optional

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,23 +3,26 @@ import { useState } from 'react'
 import ReactMapGL from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-export const Map: FC<{ width?: number; height?: number; children: ReactNode }> =
-  ({ width = 800, height = 500, children }) => {
-    const [viewport, setViewport] = useState({
-      width,
-      height,
-      latitude: 52.520952,
-      longitude: 13.400033,
-      zoom: 13,
-    })
+export const Map: FC<{
+  width?: number
+  height?: number
+  children?: ReactNode
+}> = ({ width = 800, height = 500, children }) => {
+  const [viewport, setViewport] = useState({
+    width,
+    height,
+    latitude: 52.520952,
+    longitude: 13.400033,
+    zoom: 13,
+  })
 
-    return (
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-        onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      >
-        {children}
-      </ReactMapGL>
-    )
-  }
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {children}
+    </ReactMapGL>
+  )
+}
