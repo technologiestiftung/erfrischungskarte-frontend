d958a489a9fab6327931867d59f242aeeca12438
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-06-12 14:54:59 +0200

    feat(RefreshmentMap): make activeHour changeable

---
+++ src/modules/RefreshmentMap/RefreshmentMap.types.ts
@@ -0,0 +1,16 @@
+export type TilesetFeatureKeys =
+  | '9Uhr'
+  | '10Uhr'
+  | '11Uhr'
+  | '12Uhr'
+  | '13Uhr'
+  | '14Uhr'
+  | '15Uhr'
+  | '16Uhr'
+  | '17Uhr'
+  | '18Uhr'
+  | '19Uhr'
+  | '20Uhr'
+  | '21Uhr'
+
+export type TilesetFeatureValues = 1 | 2 | 3 | 4 | 5
--- src/modules/RefreshmentMap/index.tsx
+++ src/modules/RefreshmentMap/index.tsx
@@ -4,40 +4,45 @@ import {
   PolygonLayer,
   PolygonLayerType,
 } from '../../components/Map/VectorTileLayers'
+import { TilesetFeatureKeys } from './RefreshmentMap.types'
 
-const WIND_DATA: PolygonLayerType = {
-  id: 'wind-data',
-  tileset: {
-    url: 'mapbox://technologiestiftung.9s5puknr',
-    layerName: 'wind_data-221dgh',
-  },
-  fillColorProperty: '9Uhr',
-  fillColorMap: new Map([
-    [1, 'rgba(0,68,27,100)'],
-    [2, 'rgba(42,146,74,100)'],
-    [3, 'rgba(123,200,124,100)'],
-    [4, 'rgba(202,234,195,100)'],
-    [5, 'rgba(255,255,255,0)'],
-  ]),
-}
+const defaultActiveHour: TilesetFeatureKeys = '14Uhr'
 
-const TEMPERATURE_DATA: PolygonLayerType = {
-  id: 'temperature-data',
-  tileset: {
-    url: 'mapbox://technologiestiftung.1ejpa7pd',
-    layerName: 'temperature_data-11wkut',
-  },
-  fillColorProperty: '9Uhr',
-  fillColorMap: new Map([
-    [1, 'rgba(8,48,107,100)'],
-    [2, 'rgba(40,121,185,100)'],
-    [3, 'rgba(115,179,216,100)'],
-    [4, 'rgba(200,221,240,100)'],
-    [5, 'rgba(255,255,255,0)'],
-  ]),
-}
+export const RefreshmentMap: FC<{ activeHour: TilesetFeatureKeys }> = ({
+  activeHour = defaultActiveHour,
+}) => {
+  const WIND_DATA: PolygonLayerType = {
+    id: 'wind-data',
+    tileset: {
+      url: 'mapbox://technologiestiftung.9s5puknr',
+      layerName: 'wind_data-221dgh',
+    },
+    fillColorProperty: activeHour,
+    fillColorMap: new Map([
+      [1, 'rgba(0,68,27,100)'],
+      [2, 'rgba(42,146,74,100)'],
+      [3, 'rgba(123,200,124,100)'],
+      [4, 'rgba(202,234,195,100)'],
+      [5, 'rgba(255,255,255,0)'],
+    ]),
+  }
 
-export const RefreshmentMap: FC = () => {
+  const TEMPERATURE_DATA: PolygonLayerType = {
+    id: 'temperature-data',
+    tileset: {
+      url: 'mapbox://technologiestiftung.1ejpa7pd',
+      layerName: 'temperature_data-11wkut',
+    },
+    fillColorProperty: activeHour,
+    fillColorMap: new Map([
+      [1, 'rgba(8,48,107,100)'],
+      [2, 'rgba(40,121,185,100)'],
+      [3, 'rgba(115,179,216,100)'],
+      [4, 'rgba(200,221,240,100)'],
+      [5, 'rgba(255,255,255,0)'],
+    ]),
+  }
+
   return (
     <MapRoot>
       <PolygonLayer {...WIND_DATA} />
