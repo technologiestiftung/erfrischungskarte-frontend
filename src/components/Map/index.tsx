b1aeb9cc688637d73c09e039d60bc58b5118f3e3
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-06-12 12:37:25 +0200

    fix(Map): allow children for root map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,25 +1,25 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 import { useState } from 'react'
 import ReactMapGL from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-export const Map: FC<{ width?: number; height?: number }> = ({
-  width = 800,
-  height = 500,
-}) => {
-  const [viewport, setViewport] = useState({
-    width,
-    height,
-    latitude: 52.520952,
-    longitude: 13.400033,
-    zoom: 13,
-  })
+export const Map: FC<{ width?: number; height?: number; children: ReactNode }> =
+  ({ width = 800, height = 500, children }) => {
+    const [viewport, setViewport] = useState({
+      width,
+      height,
+      latitude: 52.520952,
+      longitude: 13.400033,
+      zoom: 13,
+    })
 
-  return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    ></ReactMapGL>
-  )
-}
+    return (
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      >
+        {children}
+      </ReactMapGL>
+    )
+  }
