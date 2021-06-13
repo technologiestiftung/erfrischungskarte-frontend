0e6a580a787e4be28d0d6dc65cc3c53ecaaee883
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-06-13 20:02:35 +0200

    feat(Map): let wrapper receive ref

--- src/layouts/FullScreenMapWrapper/index.tsx
+++ src/layouts/FullScreenMapWrapper/index.tsx
@@ -1,28 +1,32 @@
-import { FC, ReactNode } from 'react'
+import { forwardRef, HTMLProps, ReactNode } from 'react'
 
-interface FullScreenMapWrapperType {
+interface FullScreenMapWrapperType extends HTMLProps<HTMLDivElement> {
   topLeft?: ReactNode
   topRight?: ReactNode
   bottomLeft?: ReactNode
   bottomRight?: ReactNode
 }
 
-export const FullScreenMapWrapper: FC<FullScreenMapWrapperType> = ({
-  topLeft,
-  topRight,
-  bottomLeft,
-  bottomRight,
-  children,
-}) => {
-  return (
-    <div className="w-screen h-screen">
-      {children}
-      {topLeft && <div className="fixed top-4 left-4">{topLeft}</div>}
-      {topRight && <div className="fixed top-4 right-4">{topRight}</div>}
-      {bottomLeft && <div className="fixed bottom-4 left-4">{bottomLeft}</div>}
-      {bottomRight && (
-        <div className="fixed bottom-4 right-4">{bottomRight}</div>
-      )}
-    </div>
-  )
-}
+export const FullScreenMapWrapper = forwardRef<
+  HTMLDivElement,
+  FullScreenMapWrapperType
+>(
+  (
+    { topLeft, topRight, bottomLeft, bottomRight, children, ...otherProps },
+    ref
+  ) => {
+    return (
+      <div className="w-screen h-screen" ref={ref} {...otherProps}>
+        {children}
+        {topLeft && <div className="fixed top-4 left-4">{topLeft}</div>}
+        {topRight && <div className="fixed top-4 right-4">{topRight}</div>}
+        {bottomLeft && (
+          <div className="fixed bottom-4 left-4">{bottomLeft}</div>
+        )}
+        {bottomRight && (
+          <div className="fixed bottom-4 right-4">{bottomRight}</div>
+        )}
+      </div>
+    )
+  }
+)
