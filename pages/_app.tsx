df50bcdb5e9d853a6b5b69c8657e15aeaf610b6b
Lucas Vogel <contact@vogelino.com>
2021-06-16 23:54:03 +0200

    style(font): Add Asap font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,6 +28,11 @@ const App: FC<{
           <title>{`${
             pageProps.title ? `${pageProps.title} - ` : ' '
           }Erfrischungskarte Berlin`}</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <Component {...pageProps} query={parsedQuery} />
       </RefreshmentMap>
