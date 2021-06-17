29f9dd0cc7df923774bcac2314517697828996dc
Lucas Soeth <lucasoeth@gmail.com>
2021-06-17 16:41:51 +0200

    refactor: update cross icon to accept props

--- src/components/Icons/Cross.tsx
+++ src/components/Icons/Cross.tsx
@@ -1,12 +1,13 @@
-import { FC } from 'react'
+import { FC, SVGProps } from 'react'
 
-export const CrossIcon: FC = () => (
+export const CrossIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
   <svg
     width="24"
     height="24"
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path
       d="M6 6L18 18M6 18L18 6L6 18Z"
