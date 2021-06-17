c5d72f47f7c43481ff4bee13f24868ee78fe84a6
Lucas Soeth <lucasoeth@gmail.com>
2021-06-17 16:54:31 +0200

    test(Splashscreen)

--- src/components/SplashScreen/SplashScreen.test.tsx
+++ src/components/SplashScreen/SplashScreen.test.tsx
@@ -1,7 +1,14 @@
 import { screen, render } from '@testing-library/react'
 import { SplashScreen } from '.'
+import * as nextRouter from 'next/router'
 
 describe('SplashScreen component', () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  nextRouter.useRouter = jest.fn().mockReturnValue({
+    query: {},
+  })
+
   it('renders the header', () => {
     render(<SplashScreen />)
     const header = screen.getByRole('heading')
@@ -12,7 +19,7 @@ describe('SplashScreen component', () => {
     render(<SplashScreen />)
     const links = screen.getAllByRole('link')
 
-    expect(links.length).toBe(3)
+    expect(links.length).toBe(5)
   })
   it('renders all the svgs', () => {
     render(<SplashScreen />)
