8247d91b51a9e597adebf6b8b57f61b56a08c463
Lucas Soeth <lucasoeth@gmail.com>
2021-06-17 15:34:00 +0200

    test: improve

--- src/components/SplashScreen/SplashScreen.test.tsx
+++ src/components/SplashScreen/SplashScreen.test.tsx
@@ -1,12 +1,23 @@
-import { render } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
 import { SplashScreen } from '.'
 
 describe('SplashScreen component', () => {
-  it('renders the splash screen', () => {
+  it('renders the header', () => {
     render(<SplashScreen />)
-    const splashScreen = document.querySelector(
-      'div.flex.flex-col.justify-center.h-full.w-full'
-    )
-    expect(splashScreen).toBeInTheDocument()
+    const header = screen.getByRole('heading')
+
+    expect(header).toBeInTheDocument()
+  })
+  it('renders all the buttons', () => {
+    render(<SplashScreen />)
+    const links = screen.getAllByRole('link')
+
+    expect(links.length).toBe(3)
+  })
+  it('renders all the svgs', () => {
+    render(<SplashScreen />)
+    const svgs = document.querySelectorAll('svg')
+
+    expect(svgs.length).toBe(3)
   })
 })
