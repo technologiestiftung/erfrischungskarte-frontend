acb1047adf3e5e6ae3b0f2a5286f61282e7a10f6
Lucas Vogel <contact@vogelino.com>
2021-06-18 17:11:25 +0200

    feat(About): Add footer

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,6 +6,7 @@ import {
   ABOUT_ACCORDION_ITEMS,
   ABOUT_INTRODUCTION_TEXT,
 } from '@modules/RefreshmentMap/content'
+import classNames from 'classnames'
 import { GetServerSideProps } from 'next'
 import React, { FC } from 'react'
 
@@ -17,23 +18,48 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => ({
   },
 })
 
+const focusStyles =
+  'focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2 focus:rounded-sm focus:ring-offset-gray-100'
 export const About: FC = () => (
   <div>
     <p className="text-gray-500 text-sm pb-6">{ABOUT_INTRODUCTION_TEXT}</p>
     <Accordion items={ABOUT_ACCORDION_ITEMS} />
-    <footer className="mt-16 flex gap-x-2 gap-y-6 flex-wrap">
-      <div className="flex flex-col flex-grow gap-3">
+    <section className="mt-16 flex gap-6 flex-wrap">
+      <div className="flex flex-col gap-3">
         <span className="text-sm">Ein Projekt der</span>
-        <TsbLogo />
+        <TsbLogo className={focusStyles} />
       </div>
-      <div className="flex flex-col flex-grow gap-3">
+      <div className="flex flex-col gap-3">
         <span className="text-sm">Durchgeführt von</span>
-        <OdisLogo />
+        <OdisLogo className={focusStyles} />
       </div>
-      <div className="flex flex-col flex-grow gap-3">
+      <div className="flex flex-col gap-3">
         <span className="text-sm">Gefördert von</span>
-        <SenWebLogo />
+        <SenWebLogo className={focusStyles} />
       </div>
+    </section>
+    <footer
+      className={classNames(
+        'mt-8 bg-gray-100 p-8 -ml-6 -mb-6 sm:-ml-8 sm:-mb-8',
+        'flex flex-wrap gap-4'
+      )}
+      style={{
+        width: 'var(--sidebarWidth, 320px)',
+      }}
+    >
+      <span className="text-xs w-full">© 2021 Technologiestiftung Berlin</span>
+      <a
+        href="https://www.technologiestiftung-berlin.de/de/impressum/"
+        className={`text-xs hover:underline ${focusStyles}`}
+      >
+        Impressum
+      </a>
+      <a
+        href="https://www.technologiestiftung-berlin.de/de/datenschutz/"
+        className={`text-xs hover:underline ${focusStyles}`}
+      >
+        Datenschutzerklärung
+      </a>
     </footer>
   </div>
 )
