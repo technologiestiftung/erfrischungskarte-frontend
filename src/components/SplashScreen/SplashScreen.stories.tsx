ecf2c2c6f43957b742e0e5c9aa639543c684434a
Lucas Vogel <contact@vogelino.com>
2021-06-21 11:55:30 +0200

    fix(Storybook): Fix broken stories with router

--- src/components/SplashScreen/SplashScreen.stories.tsx
+++ src/components/SplashScreen/SplashScreen.stories.tsx
@@ -1,13 +1,21 @@
 import { Story, Meta } from '@storybook/react'
+import { withNextRouter } from 'storybook-addon-next-router'
 
 import { SplashScreen } from '.'
 
 export default {
   title: 'Content/SplashScreen',
   component: SplashScreen,
+  decorators: [withNextRouter],
 } as Meta
 
 const Template: Story = (args) => <SplashScreen {...args} />
 
 export const Default = Template.bind({})
+Default.parameters = {
+  nextRouter: {
+    query: {},
+    pathname: '/',
+  },
+}
 Default.args = {}
