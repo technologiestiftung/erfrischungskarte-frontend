2194e5d68d69fa66eac8ac286019429ed35dbdd6
Lucas Vogel <contact@vogelino.com>
2021-06-21 11:24:11 +0200

    refactor(fonts): Import font locally instead of via google fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,11 +32,6 @@ const App: FC<{
             name="viewport"
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
           />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <Component {...pageProps} query={parsedQuery} />
       </RefreshmentMap>
