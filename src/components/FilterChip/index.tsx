f1a4d0dec702a691325b587eb8eed1a0b8f09b8a
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-06-22 12:16:40 +0200

    feat(ERF-60): crate wrapper for toggling active layers

---
+++ src/components/FilterChip/index.tsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames'
+import { FC, HTMLAttributes } from 'react'
+
+export interface FilterChipType extends HTMLAttributes<HTMLButtonElement> {
+  ariaLabel: string
+  isSelected?: boolean
+}
+
+export const FilterChip: FC<FilterChipType> = ({
+  ariaLabel,
+  isSelected = true,
+  children,
+  ...otherButtonProps
+}) => {
+  return (
+    <button
+      type="button"
+      className={classNames(
+        'px-2 py-1 border rounded-md cursor-default',
+        'focus:rounded focus:ring-2 focus:ring-gray-800 focus:outline-none',
+        'focus:ring-offset-2 focus:ring-offset-white',
+        isSelected
+          ? 'border-gray-400 text-gray-900'
+          : 'border-gray-200 text-gray-500'
+      )}
+      aria-label={ariaLabel}
+      disabled
+      {...otherButtonProps}
+    >
+      {children}
+    </button>
+  )
+}
