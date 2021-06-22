2c49fdd2c65c07e1b5e800c8e233865f56b54221
Lucas Vogel <contact@vogelino.com>
2021-06-22 15:04:57 +0200

    fix(AppTitle): Fix the mobile styles overlapping of title and controls

--- src/components/AppTitle/index.tsx
+++ src/components/AppTitle/index.tsx
@@ -5,17 +5,18 @@ import { FC } from 'react'
 export const AppTitle: FC = () => (
   <div
     className={classNames(
-      'fixed top-6 left-0 z-10',
+      'fixed top-4 sm:top-6 left-0 z-10 px-4',
       'pointer-events-none w-screen grid',
-      'place-items-center place-content-center'
+      'place-items-start place-content-start',
+      'sm:place-items-center sm:place-content-center'
     )}
   >
     <InternalLink
       href="/"
       className={classNames(
-        'italic text-2xl font-semibold',
+        'italic sm:text-2xl font-semibold',
         'rounded-lg bg-white px-3 py-2',
-        'pointer-events-auto shadow-lg',
+        'pointer-events-auto shadow-lg w-auto',
         'text-gray-800 group hover:bg-gray-200',
         'transition-colors focus:outline-none',
         'focus:ring-2 focus:ring-gray-800'
