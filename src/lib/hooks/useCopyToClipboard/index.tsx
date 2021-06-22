6479a676e9f3702d6732e22618c2901faf89e97d
Lucas Vogel <contact@vogelino.com>
2021-06-22 20:38:53 +0200

    fix(useCopyToClipboard): Add fallback for devices that dont support clipboard

--- src/lib/hooks/useCopyToClipboard/index.tsx
+++ src/lib/hooks/useCopyToClipboard/index.tsx
@@ -1,5 +1,32 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 
+function fallbackCopyTextToClipboard(
+  text: string,
+  onSuccess: () => void,
+  onError: (error: Error) => void
+): void {
+  const textArea = document.createElement('textarea')
+  textArea.value = text
+
+  // Avoid scrolling to bottom
+  textArea.style.top = '0'
+  textArea.style.left = '0'
+  textArea.style.position = 'fixed'
+
+  document.body.appendChild(textArea)
+  textArea.focus()
+  textArea.select()
+
+  try {
+    const successful = document.execCommand('copy')
+    successful && onSuccess()
+  } catch (err) {
+    onError(new Error(err))
+  }
+
+  document.body.removeChild(textArea)
+}
+
 export const useCopyToClipboard = (): {
   hasCopied: boolean
   error: Error | null
@@ -24,6 +51,7 @@ export const useCopyToClipboard = (): {
     error,
     copyToClipboard: (text) => {
       if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text, startTimeout, setError)
         return
       }
 
--- src/lib/hooks/useCopyToClipboard/useCopyToClipboard.test.tsx
+++ src/lib/hooks/useCopyToClipboard/useCopyToClipboard.test.tsx
@@ -27,13 +27,18 @@ const createTestComponent = (
 }
 
 const writeText = jest.fn()
+const execCommand = jest.fn()
 Object.assign(navigator, {
   clipboard: {
     writeText: writeText,
   },
 })
+Object.assign(document, {
+  execCommand: execCommand,
+})
 
 describe('useCopyToClipboard', () => {
+  jest.spyOn(navigator.clipboard, 'writeText')
   test('should copy if navigator exists', async () => {
     const onSuccess = jest.fn()
     const onError = jest.fn()
@@ -64,19 +69,67 @@ describe('useCopyToClipboard', () => {
       expect(onError).toHaveBeenCalled()
     })
   })
-  test('should do nothing if the clipboard is not supported', async () => {
+  test('should call fallback with success if clipboard is not supported', async () => {
     const onSuccess = jest.fn()
     const onError = jest.fn()
     writeText.mockRejectedValue(new Error())
+    execCommand.mockReturnValue(true)
     const originalClipboard = navigator.clipboard
     Object.assign(navigator, { clipboard: undefined })
     const TestComponent = createTestComponent(onSuccess, onError)
     render(<TestComponent />)
     Object.assign(navigator, originalClipboard)
 
-    await waitFor(() => {
-      expect(onSuccess).not.toHaveBeenCalledWith(true)
-      expect(onError).not.toHaveBeenCalled()
+    await waitFor(
+      () => {
+        expect(onSuccess).toHaveBeenNthCalledWith(1, false)
+        expect(onSuccess).toHaveBeenNthCalledWith(2, true)
+        expect(onSuccess).toHaveBeenNthCalledWith(3, false)
+        expect(onError).not.toHaveBeenCalled()
+      },
+      { timeout: 3100 }
+    )
+  })
+  test('should call nothing if fallback execCommand return false', async () => {
+    const onSuccess = jest.fn()
+    const onError = jest.fn()
+    writeText.mockRejectedValue(new Error())
+    execCommand.mockReturnValue(false)
+    const originalClipboard = navigator.clipboard
+    Object.assign(navigator, { clipboard: undefined })
+    const TestComponent = createTestComponent(onSuccess, onError)
+    render(<TestComponent />)
+    Object.assign(navigator, originalClipboard)
+
+    await waitFor(
+      () => {
+        expect(onSuccess).toHaveBeenCalledWith(false)
+        expect(onSuccess).toHaveBeenCalledTimes(1)
+        expect(onError).not.toHaveBeenCalled()
+      },
+      { timeout: 3100 }
+    )
+  })
+  test('should call fallback with fail if clipboard is not supported', async () => {
+    const onSuccess = jest.fn()
+    const onError = jest.fn()
+    writeText.mockRejectedValue(new Error())
+    execCommand.mockImplementation(() => {
+      throw new Error('Error')
     })
+    const originalClipboard = navigator.clipboard
+    Object.assign(navigator, { clipboard: undefined })
+    const TestComponent = createTestComponent(onSuccess, onError)
+    render(<TestComponent />)
+    Object.assign(navigator, originalClipboard)
+
+    await waitFor(
+      () => {
+        expect(onSuccess).toHaveBeenCalledWith(false)
+        expect(onSuccess).toHaveBeenCalledTimes(1)
+        expect(onError).toHaveBeenCalled()
+      },
+      { timeout: 3100 }
+    )
   })
 })
