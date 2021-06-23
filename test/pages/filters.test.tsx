4775b8eb941ba6a31cde13edf8e0d8934571dfcc
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-06-23 13:41:05 +0200

    test(/filters): test existence of support note

--- test/pages/filters.test.tsx
+++ test/pages/filters.test.tsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
 import * as nextRouter from 'next/router'
 import { Filters } from '../../pages/filters'
+import * as hasWebPSupport from '@lib/hooks/useHasWebPSupport'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -17,8 +18,28 @@ jest.mock('next/image', () => ({ src, alt }) => {
   return <img src={src} alt={alt} />
 })
 
+const useHasWebPSupport = jest.fn()
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+hasWebPSupport.useHasWebPSupport = useHasWebPSupport
+
 describe('Filters page', () => {
   it('renders without crashing', () => {
+    useHasWebPSupport.mockReturnValue(true)
+
     render(<Filters query={{}} />)
+
+    const supportNote = screen.queryByText(/Leider können die Schatten/i)
+    expect(supportNote).not.toBeInTheDocument()
+  })
+
+  it('displays a note if webp is not supported', () => {
+    useHasWebPSupport.mockReturnValue(false)
+
+    render(<Filters query={{}} />)
+
+    const supportNote = screen.getByText(/Leider können die Schatten/i)
+    expect(supportNote).toBeInTheDocument()
   })
 })
