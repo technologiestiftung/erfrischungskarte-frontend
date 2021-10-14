14eafb441702b5767471dcbb00cef2be839144e8
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-10-14 13:42:47 +0200

    chore: add @content alias

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   verbose: true,
-	testEnvironment: 'jsdom',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
-	moduleNameMapper: {
-		'^@components(.*)$': '<rootDir>/src/components$1',
-		'^@lib(.*)$': '<rootDir>/src/lib$1',
-		'^@modules(.*)$': '<rootDir>/src/modules$1',
-		'^@layouts(.*)$': '<rootDir>/src/layouts$1',
-		'\\.css$': '<rootDir>/test/__mocks__/cssMock.ts',
+  moduleNameMapper: {
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@lib(.*)$': '<rootDir>/src/lib$1',
+    '^@modules(.*)$': '<rootDir>/src/modules$1',
+    '^@layouts(.*)$': '<rootDir>/src/layouts$1',
+    '^@content(.*)$': '<rootDir>/src/content$1',
+    '\\.css$': '<rootDir>/test/__mocks__/cssMock.ts',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.ts',
-	}
+  },
 }
