9c208336dc203f292faaa3b1880b8c22442abda4
annameide <46717848+annameide@users.noreply.github.com>
2024-07-29 14:33:16 +0200

    adjust colors for betterr contrast and accessibility

--- src/components/PoiLegendItem/index.tsx
+++ src/components/PoiLegendItem/index.tsx
@@ -16,7 +16,7 @@ export const PoiLegendItem: FC<PoiLegendItemType> = ({
     <div className="flex items-center" {...otherProps}>
       <span
         // here the size of the circle and the border are set
-        className="inline-block w-4 h-4 self-baseline rounded-full shadow-md border-4"
+        className="inline-block w-4 h-4 self-baseline rounded-full shadow-md border-2"
         style={{ backgroundColor: fill, borderColor: border }}
       ></span>
       <span className="ml-1 text-gray-700 text-sm">{label}</span>
