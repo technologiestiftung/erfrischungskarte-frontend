05dcf76e08d2722e3b0e57cd125f0daadc65ae6e
hhack <hans.f.hack@googlemail.com>
2024-07-30 06:14:31 +0200

    new colors

--- src/components/PoiLegendItem/index.tsx
+++ src/components/PoiLegendItem/index.tsx
@@ -13,7 +13,7 @@ export const PoiLegendItem: FC<PoiLegendItemType> = ({
   return (
     <div className="flex items-center" {...otherProps}>
       <span
-        className="inline-block w-4 h-4 self-baseline rounded-full shadow-md border-4 border-white"
+        className="inline-block w-4 h-4 self-baseline rounded-full shadow-md border-2 border-white"
         style={{ backgroundColor: color }}
       ></span>
       <span className="ml-1 text-gray-700 text-sm">{label}</span>
